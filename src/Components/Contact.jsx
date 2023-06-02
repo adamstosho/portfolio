0f744b7ba4f5f093b7784653223b936d92d7f05c
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-06-02 16:36:51 +0100

    fixes

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
         <p className='text-primary1 text-[13px]'><span className='text-primary1'>04.</span> What&lsquo;s Next?</p>
         <h1 className='text-[2rem] font-bold text-var_color'>Get In Touch</h1>
         <p className='lg:w-[60%] text-center'> I&rsquo;m currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I&lsquo;ll try my best to get back to you!</p>
-        <p className={styles.sayHello}></p>
+       <a href=""><p className={styles.sayHello}></p></a>
         
     </div>
   )
