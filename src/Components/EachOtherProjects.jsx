f03306d52c94ba0b665545dcbdc2d1935a1f22ad
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-06-04 01:37:09 +0100

    fixes

--- src/Components/EachOtherProjects.jsx
+++ src/Components/EachOtherProjects.jsx
@@ -5,45 +5,50 @@ const EachOtherProjects = ({
     lang2,
     lang3,
     lang4,
+    live,
+    github,
+    title,
+    desc,
+
 }) => {
   return (
     <div className='lg:max-w-[32%] max-w-full flex flex-col gap-4 p-6 bg-primary_bg2 shadow-lg rounded-lg'>
         <div className='flex items-center justify-between'>
             <div>
             <Image
-            src={"/github-line.png"}
+            src={"/file.png"}
             height={50}
             width={50}
             alt={'File'}
             />
             </div>
             <div className={`clickales flex gap-4`}>
-                    <a href="" target='blank'>
+                    <a href={live} target='blank'>
                     <Image
                     src={"/live2.png"}
-                    height={25}
-                    width={25}
+                    height={30}
+                    width={30}
                     alt='icon1'
                     />
                     </a>
-                    <a href="" target='blank'>
+                    <a href={github} target='blank'>
                      <Image
                     src={"/github-line.png"}
                     height={25}
-                    width={25}
+                    width={30}
                     alt='icon2'
                     />
                     </a>
                 </div>
         </div>
     <div className='flex flex-col gap-3'>
-        <h1 className='text-var_color font-[700] '>Integration of Algolia Search width Wordpress Multisite</h1>
-        <p className='text-[13px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quas, </p>
+        <h1 className='text-var_color font-[700] '>{title}</h1>
+        <p className='text-[13px]'>{desc}</p>
         <div className="materialUsed text-[13px] flex justify-around">
-                    <p>{'lang1'}</p>
-                    <p>{'lang2'}</p>
-                    <p>{'lang3'}</p>
-                    <p>{'lang4'}</p>
+                    <p>{lang1}</p>
+                    <p>{lang2}</p>
+                    <p>{lang3}</p>
+                    <p>{lang4}</p>
                 </div>
     </div>
     </div>
--- src/Components/OtherProject.jsx
+++ src/Components/OtherProject.jsx
@@ -4,13 +4,45 @@ import styles from "../styles/SomeProjects.module.css"
 const OtherProject = () => {
     const otherProjectArr = [
         {
-            title:'Integration of Algolia Search width Wordpress Multisite',
-            description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quas, ',
-            lang1:'Html',
-            lang2:'Css',
-            lang3:'TypeScript',
-            lang4:'Redux',
-            }
+            lang1:'React-Redux',
+            lang2:'',
+            lang3:'TailwindCss',
+            lang4:'Netlify',
+            title:'Task-List',
+            desc:'A Task List to make proper decision. Where users can add a new task and mark or delete any task that has been completed.',
+            github:'https://github.com/Intelligence247/Redux_To_Do_List',
+            live:'https://creative-gelato-39395b.netlify.app',
+            },
+            {
+              lang1:'HTML/CSS',
+              lang2:'',
+              lang3:'Vanilla.js',
+              lang4:'Netlify',
+              title:'Cake-Shop',
+              desc:'Time to go for shopping!. This website gives users the opportunity to search for what they want to order for.',
+              github:'https://github.com/Intelligence247/cake-shop',
+              live:'https://ephemeral-pithivier-dc232a.netlify.app/',
+              },
+              {
+                lang1:'Reactjs',
+                lang2:'',
+                lang3:'TailwindCSS',
+                lang4:'Netlify',
+                title:'Temperature Converter',
+                desc:'This App can convert any given temperature to another form. Either from Celsius to Fahreint or any other possible forms.',
+                github:'https://github.com/Intelligence247/TemperatureConverter',
+                live:'https://cute-meerkat-9c5153.netlify.app/',
+                },
+                {
+                  lang1:'Reactjs',
+                  lang2:'',
+                  lang3:'TailwindCSS',
+                  lang4:'Netlify',
+                  title:'World Country Visualization',
+                  desc:'This App visualizes all the countries in the world with their populations, capital e.t.c. Users can search any country with either the name of the country or the capital',
+                  github:'https://github.com/Intelligence247/asabeneh-world-countries-data-project',
+                  live:'https://stately-kataifi-5e13a1.netlify.app/',
+                  },
     ]
   return (
     <div className={`w-full flex flex-col gap-8 items-center mt-40 ${styles.otherProjectW}`}>
@@ -19,12 +51,20 @@ const OtherProject = () => {
         <p className='text-primary1 cursor-pointer'><a href="">View the archive</a></p>
         </div>
         <div className={` ${styles.Otherprojects} flex justify-between lg:flex-row flex-col flex-wrap gap-5`}>
-        <EachOtherProjects/>
-        <EachOtherProjects/>
-        <EachOtherProjects/>
-        <EachOtherProjects/>
-        <EachOtherProjects/>
-        <EachOtherProjects/>
+          {
+            otherProjectArr.map((other)=>(
+              <EachOtherProjects
+              lang1={other.lang1}
+              lang2={other.lang2}
+              lang3={other.lang3}
+              lang4={other.lang4}
+              title={other.title}
+              desc={other.desc}
+              github={other.github}
+              live={other.live}
+              />
+            ))
+          }
         </div>
         <p className={styles.showMore}></p>
     </div>
