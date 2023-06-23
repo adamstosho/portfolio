89b62c5626bba7a9a382faa80628632067e27e37
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-06-24 00:17:19 +0100

    Expertise added

--- src/Components/Expertise.jsx
+++ src/Components/Expertise.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
 import styles from "../styles/Expertise.module.css"
 import Headings from './Headings'
+import Image from 'next/image'
 
 const Expertise = () => {
-  
+  const expertiseObjArr = [
+    {
+      src:'html5.svg',
+      alt: 'HTML5 Logo',
+    },
+    {
+      src:'css3.svg',
+      alt: 'css3 Logo',
+    },
+    {
+      src:'javascript.svg',
+      alt: 'JavaScript Logo',
+    },
+    {
+      src:'tailwindcss.svg',
+      alt: 'tailwindcss Logo',
+    },
+    {
+      src:'reactjs.svg',
+      alt: 'reactjs Logo',
+    },
+    {
+      src:'redux.svg',
+      alt: 'redux Logo',
+    },
+    {
+      src:'vite.svg',
+      alt: 'vite Logo',
+    },
+    {
+      src:'npm.svg',
+      alt: 'vite Logo',
+    },
+    
+  ]
   return (
     <div className={styles.ExpertiseW} id='experience'>
         <Headings
@@ -14,6 +49,17 @@ const Expertise = () => {
         <h3>{`Here are a few technologies I’ve been working with recently:`}</h3>
 
 
+<div className={styles.expertiseImages}>
+         {
+          expertiseObjArr.map((expert)=>(
+            <Image
+            src={expert.src} 
+            width={100}
+            height={100}
+            alt={expert.alt} />
+          ))
+         }
+</div>
         {/* <div className={styles.coreSkills}>
         <h1 className='text-primary1 text-center text-2xl mt-4'>_____ Core Skills</h1>
         <p>HTML</p>
