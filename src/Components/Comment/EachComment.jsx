bc5d3a73eca117329fb1050593368524ebeb5789
Intelligence247 <ua67527056@gmail.com>
2024-01-22 16:43:06 +0100

    reply to comments and some other fixes

--- src/Components/Comment/EachComment.jsx
+++ src/Components/Comment/EachComment.jsx
@@ -1,30 +1,59 @@
 import React from "react";
 import styles from "../../styles/Comment.module.css";
-import { Circles } from "react-loader-spinner";
+import { Blocks, Circles } from "react-loader-spinner";
 
 const EachComment = ({ apiData, Loading }) => {
+  const [active, setActive] = React.useState(false)
+
   return (
     <div className={`${styles.displayComments}  w-full`}>
       {apiData !== null && apiData.length >= 1 && !Loading ? (
         apiData.map((a, i) => (
-          <div
-          key={i}
-            className={`${styles.eachComment} flex items-start gap-2 border-b-[1px] border-b-blur_texts py-2 mb-2`}
-          >
+          <div className="border-b-[1px] border-b-blur_texts py-2 mb-2" key={i}>
             <div
-              className={`${styles.img}  border-primary1 border-[1.5px] border-b-[1px] border-b-blur_texts animate-pulse rounded-full bg-primary_bg2 text-white flex justify-center items-center lg:text-3xl `}
+              className={`${styles.eachComment} flex items-start gap-2`}
             >
-              {a.name.slice(0, 1).toUpperCase()}
-            </div>
-            <div className="content">
-              <div className="name flex gap-2">
-                <strong className="font-[500]">
-                  <span className="text-var_color hover:text-var_color/50"> {a.name}</span> {a.comment}
-                </strong>{" "}
+              <div
+                className={`${styles.img}  border-primary1 border-[1.5px] border-b-[1px] border-b-blur_texts animate-pulse rounded-full bg-primary_bg2 text-white flex justify-center items-center lg:text-3xl `}
+              >
+                {a.name.slice(0, 1).toUpperCase()}
               </div>
-              <div className="reply flex gap-4 lg:gap-8 pt-1">
-                <p>{a.date.slice(0, 4)}</p>
-                <p>Reply</p>
+              <div className="content">
+                <div className="name flex gap-2">
+                  <strong className="font-[500]">
+                    <span className="text-var_color hover:text-var_color/50">
+                      {" "}
+                      {a.name}
+                    </span>{" "}
+                    {a.comment}
+                  </strong>{" "}
+                </div>
+                <div className="reply flex gap-4 lg:gap-8 pt-1">
+                  <p>{a.date.slice(0, 4)}</p>
+                  <p onClick={()=> {setActive(!active)}} className={`${a.reply ==="" || a.reply.length <=0 ?"hidden": "flex"} hover:underline cursor-pointer `}>{!active?"View":"Close"}-Reply</p>
+                </div>
+              </div>
+            </div>
+            {/* Relpys */}
+            <div className={`${styles.eachComment} ${a.reply ==="" || a.reply.length <=0 ?"hidden": "flex"} ${active?"h-max":"h-0 overflow-hidden"} scale-[0.9] flex items-start gap-2 `}>
+              <div
+                className={`${styles.img}  border-primary1 border-[1.5px] border-b-[1px] border-b-blur_texts animate-pulse rounded-full bg-primary_bg2 text-white flex justify-center items-center lg:text-3xl `}
+              >
+                A
+              </div>
+              <div className="content">
+                <div className="name flex gap-2">
+                  <strong className="font-[500]">
+                    <span className="text-var_color hover:text-var_color/50">
+                     Admin
+                    </span>{" "}
+                    {a.reply}
+                  </strong>{" "}
+                </div>
+                <div className="reply flex gap-4 lg:gap-8 pt-1">
+                  <p>{a.date.slice(0, 4)}</p>
+                  <p></p>
+                </div>
               </div>
             </div>
           </div>
--- src/app/ProjectDetails/page.jsx
+++ src/app/ProjectDetails/page.jsx
@@ -8,6 +8,7 @@ import { Circles } from "react-loader-spinner";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Headings from "@/Components/Headings";
+import styles from "./page.module.css"
 
 const ProjectDetailsComponent = () => {
   const [apiData, setApiData] = useState([]);
@@ -85,7 +86,8 @@ const ProjectDetailsComponent = () => {
           theme: "dark",
         });
         setuserComment({ name: "", email: "", comment: "" });
-        setComments(response.data, ...comments);
+        setComments((comments)=>[response.data, ...comments]);
+
       } catch (err) {
         setLoading(false);
         console.log(err);
@@ -109,7 +111,6 @@ const ProjectDetailsComponent = () => {
 
   useEffect(() => {
     const selectedID = localStorage.getItem("selectedID");
-
     const url = `https://riganapi.pythonanywhere.com/api/v2/comments/get_comments/?project_id=${selectedID}&api_token=${API_KEY}`;
     const fetchDataForPage1 = async () => {
       setLoading2(true);
@@ -126,15 +127,16 @@ const ProjectDetailsComponent = () => {
     fetchDataForPage1();
   }, []);
 
+console.log(comments)
   return (
-    <div className='h-screen w-full bg-primary_bg bg-[url("/arrowdown3.png")]'>
+    <div className='w-full bg-primary_bg bg-[url("/arrowdown3.png")]'>
       {message === "success" ? (
-        <div className='h-auto w-full bg-primary_bg bg-[url("/arrowdown3.png")] flex flex-col items-center gap-8 py-6 px-20'>
-          <h1 className="text-primary1 text-4xl font-bold">{apiData.title}</h1>
+        <div className='h-auto w-full bg-primary_bg bg-[url("/arrowdown3.png")] flex flex-col items-center gap-8 py-6 lg:px-20 px-4'>
+          <h1 className="text-primary1 lg:text-4xl text-2xl font-bold text-center">{apiData.title}</h1>
           <div className="w-full">
             <h3>Used Technology</h3>
-            <section className="grid grid-cols-2 justify-center items-center">
-              <ul className="grid grid-cols-2 gap-y-2 w-3/4">
+            <section className="lg:grid grid-cols-2 justify-center items-center">
+              <ul className="lg:grid grid-cols-2 gap-y-2 lg:w-3/4 w-full">
                 {apiData.frameworks.map((f, i) => (
                   <li key={i}>
                     <span className="text-primary1 font-bold text-xl">▹</span>{" "}
@@ -142,7 +144,7 @@ const ProjectDetailsComponent = () => {
                   </li>
                 ))}
               </ul>
-              <div
+              <div className="lg:text-start text-center py-8"
                 contentEditable="false"
                 dangerouslySetInnerHTML={{
                   __html: apiData.description,
@@ -171,14 +173,13 @@ const ProjectDetailsComponent = () => {
               </a>
             </div>
           </section>
-          <section className="commentSection w-3/5">
-            {/* <h1 className="text-3xl">Comment Section</h1> */}
-            <Headings text={`Comment section`} />
-            <div className="overflow-y-auto h-[25rem] scroll-m-0">
+          <section className="commentSection lg:w-3/5 w-full">
+            <Headings width={'w-20'} text={`Comment section`} />
+            <div className={`${styles.viewPageComment} overflow-y-auto h-[15rem] flex justify-center ${comments.length >= 1? "items-start": 'items-center'} scroll-m-0`}>
               <EachComment apiData={comments} Loading={Loading2} />
             </div>
             <div className="flex flex-col gap-3 border-t-[1px] border-blur_texts pt-2 px-2  ">
-              <div className="inputTop flex  items-center justify-around">
+              <div className="inputTop flex lg:flex-row gap-4 flex-col  items-center justify-around">
                 <input
                   required
                   type="text"
